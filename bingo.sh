4d4b0bc3a0e666faf32b20783b1f2eadf2be0781
sherali <sheraliakbarov699-lang>
2026-08-14 05:13:43 +0000

    Add bingo.sh generator for level 3

---
+++ bingo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Bingo Number Generator
+
+echo -e "\n~~ Bingo Number Generator ~~\n"
+
+# 1-75 oralig'idagi tasodifiy sonni olish
+NUMBER=$(( RANDOM % 75 + 1 ))
+
+TEXT="The next number is, "
+
+# Harf va son juftligini aniqlash shartlari
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
+elif [[ $NUMBER -lt 61 ]]
+then
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT O:$NUMBER
+fi
