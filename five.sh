744a87629ee82e430f46b1922859a5a0cda2f43d
sherali <sheraliakbarov699@gmail.com>
2026-08-19 07:38:43 +0000

    Update script paths in five.sh

--- five.sh
+++ five.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Program to run my other four programs
 
-./questionnaire.sh
-./countdown.sh 3
-./bingo.sh
-./fortune.sh
+./1.questionnaire.sh
+./2.countdown.sh 3
+./3.bingo.sh
+./4.fortune.sh
